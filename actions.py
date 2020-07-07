12b003ec99010917e21bc5345e229eba609d8a35
Marci <matusekma@gmail.com>
2020-07-07 08:36:59 +0200

    Create form

--- actions.py
+++ actions.py
@@ -25,3 +25,43 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+from typing import Dict, List, Text, Any, Union
+
+
+class SalesForm(FormAction):
+    """Collect sales information and adds it to the spreadsheet"""
+
+    def name(self):
+        return "sales_form"
+
+    @staticmethod
+    def required_slots(tracker):
+        return [
+            "job_function",
+            "use_case",
+            "budget",
+            "person_name",
+            "company",
+            "business_email",
+        ]
+
+    def submit(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        dispatcher.utter_message("Thanks for getting in touch, we’ll contact you soon")
+        return []
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
+        """A dictionary to map required slots to
+        - an extracted entity
+        - intent: value pairs
+        - a whole message
+        or a list of them, where a first match will be picked"""
+        return {"use_case": self.from_text(intent="inform")}
--- data/stories.md
+++ data/stories.md
@@ -13,3 +13,9 @@
  ## Some question from FAQ
 * faq
   - respond_faq
+  
+## sales form
+* contact_sales
+  - sales_form                   <!--Run the sales_form action-->
+  - form{"name": "sales_form"}   <!--Activate the form-->
+  - form{"name": null}           <!--Deactivate the form-->
